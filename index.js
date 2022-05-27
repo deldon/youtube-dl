242b6985188c5001bb7287add1231a40002b798a
deldon <r.deldon@gmail.com>
2022-05-27 10:47:05 +0200

    fix format

--- index.js
+++ index.js
@@ -48,13 +48,6 @@ io.on('connection', (socket) => {
     });
 });
 
-
-
-
-
-
-
-
 server.listen(3000, () => {
     console.log(`Example app listening on port 3000`)
 })
--- public/js/app.js
+++ public/js/app.js
@@ -3,35 +3,34 @@ var socket = io();
 var form = document.getElementById('form');
 var input = document.getElementById('input');
 
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function (e) {
 
     const card = document.getElementById('card');
     const info = document.createElement('div');
     info.id = 'info'
     card.appendChild(info)
 
-  e.preventDefault();
-  form.innerHTML = '';
-  if (input.value) {
-      console.log(input.value);
-    socket.emit('chat message', input.value);
-    input.value = '';
-  }
+    e.preventDefault();
+    form.innerHTML = '';
+    if (input.value) {
+        console.log(input.value);
+        socket.emit('chat message', input.value);
+        input.value = '';
+    }
 });
 
-socket.on('chat message', function(msg) {
-  const info = document.getElementById('info')
-  const url = document.getElementById('url');
-  
+socket.on('chat message', function (msg) {
+    const info = document.getElementById('info')
+    const url = document.getElementById('url');
 
+    if (msg.info != 'null') {
+        info.textContent = msg.info;
+        info.style.width = msg.info
+    }
+    
+    console.log(msg.media);
 
-  if (msg.info != 'null') {
-      info.textContent = msg.info;
-      info.style.width = msg.info
-  }
-console.log(msg.media);
-  
-  if (msg.media != 'undefined' && msg.media != 'mp3') {
+    if (msg.media != 'undefined' && msg.media != 'mp3') {
         const download = document.createElement('a');
         info.style.display = 'none'
 
@@ -41,6 +40,6 @@ console.log(msg.media);
 
         url.appendChild(download)
 
-  }
+    }
 
 });
